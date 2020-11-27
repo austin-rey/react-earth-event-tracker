66973e1d893f0d1ce807ac5ea9571bb6e1e53b3c
austin-rey <austinrey95@gmail.com>
2020-11-27 15:00:05 -0600

    Added local state to event categories and added material ui

--- src/components/EventsByCategories.js
+++ src/components/EventsByCategories.js
@@ -1,31 +1,87 @@
 import PropTypes from 'prop-types'
+import {useState,useEffect} from 'react';
+
+import {Card,CardHeader,IconButton,CardContent,List,ListItem,ListItemText } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+import Moment from 'react-moment';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex'
+    },
+    cardContainer: {
+        borderRadius: '0px',
+        margin: '10px'
+    },
+    cardHeader: {
+       backgroundColor: '#D9D9D9',
+       color: '#353535'
+    },
+    answerList: {
+        display: 'flex',
+        flexDirection:"column",
+        alignItems: 'flex-start',
+        borderLeft: '1px solid #BEE3DB',
+        marginLeft: '10px'
+    },
+    cardActions: {
+        justifyContent: 'flex-end',
+        flexDirection: 'row'
+    }
+}));
 
 const EventsByCategories = ({eventData,categoryData}) => {
+    const classes = useStyles();
 
-    console.log(eventData);
+    const [categorizedEvents, setCategorizedEvents] = useState([]);
+    useEffect(() => {
+        const getEventsByCategory = () =>{
+            categoryData.forEach(async (cat) => {
+                const eventsByCategory = await fetch(`https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/${cat.id}`);
+                const res = await eventsByCategory.json();
+                setCategorizedEvents(categorizedEvents => [...categorizedEvents, res]);
+            })
+        }
+        getEventsByCategory();
+    },[categoryData])
 
-    const categories = categoryData.map((category => {
-        return <div className="card-content-item">
-            <h3>{category.title}</h3>
-            <p>{category.description}</p>
-            {eventData.map(event=> {
-                if(event.categories[0].id === category.id){
-                    return <p>{event.title}</p>
-                }
+    // console.log(categorizedEvents);
 
+    const categories = categorizedEvents.map((category => {
+        console.log(category);
+        return <Card className={classes.cardContainer} variant="outlined">
+        <CardHeader
+            className={classes.cardHeader}
+            title={category.title.replace('EONET Events: ','')}
+            subheader={category.description}
+        />
+        <CardContent>
+            <List>
+                <ListItem className={classes.answerList}>
+                {
+                category.events.map((event,index)=> {
+                   return <ListItemText primary={event.title} secondary={<Moment format="YYYY/MM/DD - hh:mm:ss">{event.geometries[0].date}</Moment>} />
             })}
-        </div>
+            {category.events.length == 0 && <p className="no-results">No events found.</p>}
+                </ListItem>
+            </List>
+        </CardContent>
+    </Card>
     }));
 
     return (
-        <div className="card">
-            <div className="card-header">
-                <h1>Events by Category</h1>
-            </div>
-            <div className="card-body">
-                {categories}
-            </div>
-        </div>
+    <div>
+        <header className="header">
+            <h2>Events by Category</h2>
+
+        </header>
+        {categories}
+
+    </div>
+
     )
 }
 
